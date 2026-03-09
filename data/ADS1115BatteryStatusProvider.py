d24fdc5f7f76b0b7a4ea00e325af03ed17119e51
Ian Ramos <juventuddisponible@gmail.com>
2026-03-09 15:21:13 -0500

    Fix ADS1115 battery voltage scaling for divider

--- data/ADS1115BatteryStatusProvider.py
+++ data/ADS1115BatteryStatusProvider.py
@@ -10,15 +10,18 @@ from data.BatteryStatusProvider import BatteryStatusProvider
 
 logger = logging.getLogger('battery_data')
 
+
 class ADS1115BatteryStatusProvider(BatteryStatusProvider):
 
     __CONVERSION_REG = 0x00
     __CONFIG_REG = 0x01
 
-    __FSR = 6.144 # full scale range
+    __FSR = 6.144  # full scale range
 
     __V_CHARGED = 4.2
     __V_DISCHARGED = 2.9
+
+    __DIVIDER_RATIO = 2.0  # 10k / 10k divider, battery voltage is tap * 2
 
     __last_levels = deque(maxlen=10)
 
@@ -29,41 +32,52 @@ class ADS1115BatteryStatusProvider(BatteryStatusProvider):
 
     def __read_channel(self, channel=0) -> float:
         config = [
-            0b11000001 | (channel << 4), # GND as reference, select channel
-            0b10000011                   # default settings
+            0b11000001 | (channel << 4),  # single-ended, select channel
+            0b10000011
         ]
         self.__bus.write_i2c_block_data(self.__address, self.__CONFIG_REG, config)
         time.sleep(0.1)
         word_data = self.__bus.read_word_data(self.__address, self.__CONVERSION_REG)
         self.__device_status = DeviceStatus.OPERATIONAL
-        # swap bytes in word
+
+        # swap bytes
         raw_data = ((word_data & 0xFF) << 8) | (word_data >> 8)
         logger.debug(f'{raw_data=}')
-        # 0x000 - 0x7FFF -> 0 ... +FSR | 0x8000 - 0xFFFF -> -FSR ... 0
-        return (raw_data if raw_data < 0x8000 else raw_data - 0x10000) * self.__FSR / 0x8000
+
+        measured_voltage = (
+            (raw_data if raw_data < 0x8000 else raw_data - 0x10000)
+            * self.__FSR / 0x8000
+        )
+        return measured_voltage
 
     @override
     def get_state_of_charge(self) -> float:
         try:
-            voltage = self.__read_channel()
-            logger.debug(f'{voltage=}')
-            if voltage < 1.5:
+            tap_voltage = self.__read_channel()
+            battery_voltage = tap_voltage * self.__DIVIDER_RATIO
+
+            logger.debug(f'{tap_voltage=}, {battery_voltage=}')
+
+            # detect disconnected / invalid reading
+            if tap_voltage <= 0.05:
                 self.__device_status = DeviceStatus.NO_DATA
                 return 0.0
+
+            self.__last_levels.append(battery_voltage)
+            smoothed_voltage = sum(self.__last_levels) / len(self.__last_levels)
+
+            if smoothed_voltage < self.__V_DISCHARGED:
+                return 0.0
+            elif smoothed_voltage > self.__V_CHARGED:
+                return 1.0
             else:
-                self.__last_levels.append(voltage)
-                smoothed_voltage = sum(self.__last_levels) / len(self.__last_levels)
-                if smoothed_voltage < self.__V_DISCHARGED:
-                    return 0
-                elif smoothed_voltage > self.__V_CHARGED:
-                    return 1
-                else:
-                    return (smoothed_voltage - self.__V_DISCHARGED) / (self.__V_CHARGED - self.__V_DISCHARGED)
+                return ((smoothed_voltage - self.__V_DISCHARGED) /
+                        (self.__V_CHARGED - self.__V_DISCHARGED))
+
         except OSError as e:
             logger.warning(e)
             self.__device_status = DeviceStatus.UNAVAILABLE
             return 0.0
-
 
     @override
     def get_device_status(self) -> DeviceStatus:
